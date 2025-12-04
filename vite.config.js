1ad2019f3f2b51fea96f5890115f5568d925c03b
kevlarveil <kevlarveil@gmail.com>
2025-12-04 01:32:04 +0100

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
   base: '/emoji-crypto-chat/',
+  root: path.resolve(__dirname, '.'),
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
